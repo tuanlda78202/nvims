5d3ecab85bb30c4a7a391445231d1a9b174ec1df
charles <tuanleducanh78202@gmail.com>
2026-03-20 02:25:57 +0700

    fix: buffer close tab + panel

--- lua/nvims/plugins/bufferline.lua
+++ lua/nvims/plugins/bufferline.lua
@@ -6,6 +6,16 @@ return {
 	opts = {
 		options = {
 			mode = "tabs",
+			close_command = function(n)
+				if vim.fn.tabpagenr("$") > 1 then
+					vim.cmd("tabclose " .. n)
+				end
+			end,
+			right_mouse_command = function(n)
+				if vim.fn.tabpagenr("$") > 1 then
+					vim.cmd("tabclose " .. n)
+				end
+			end,
 			separator_style = "slant",
 		},
 	},
--- lua/nvims/plugins/surround.lua
+++ lua/nvims/plugins/surround.lua
@@ -2,19 +2,32 @@ return {
 	"kylechui/nvim-surround",
 	event = { "BufReadPre", "BufNewFile" },
 	version = "*",
-	opts = {
-		keymaps = {
-			insert = false,
-			insert_line = false,
-			normal = "<leader>sa",
-			normal_cur = false,
-			normal_line = "<leader>sA",
-			normal_cur_line = false,
-			visual = "<leader>sa",
-			visual_line = "<leader>sA",
-			delete = "<leader>sd",
-			change = "<leader>sc",
-			change_line = "<leader>sC",
-		},
-	},
+	init = function()
+		vim.g.nvim_surround_no_mappings = true
+	end,
+	config = function()
+		require("nvim-surround").setup({})
+
+		vim.keymap.set("n", "<leader>sa", "<Plug>(nvim-surround-normal)", {
+			desc = "Surround add (motion)",
+		})
+		vim.keymap.set("n", "<leader>sA", "<Plug>(nvim-surround-normal-line)", {
+			desc = "Surround add (line)",
+		})
+		vim.keymap.set("x", "<leader>sa", "<Plug>(nvim-surround-visual)", {
+			desc = "Surround add (selection)",
+		})
+		vim.keymap.set("x", "<leader>sA", "<Plug>(nvim-surround-visual-line)", {
+			desc = "Surround add (selection line)",
+		})
+		vim.keymap.set("n", "<leader>sd", "<Plug>(nvim-surround-delete)", {
+			desc = "Surround delete",
+		})
+		vim.keymap.set("n", "<leader>sc", "<Plug>(nvim-surround-change)", {
+			desc = "Surround change",
+		})
+		vim.keymap.set("n", "<leader>sC", "<Plug>(nvim-surround-change-line)", {
+			desc = "Surround change (line)",
+		})
+	end,
 }
