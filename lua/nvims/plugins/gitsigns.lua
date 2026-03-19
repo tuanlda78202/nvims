8555adcd7418a0a256151d29ab8d46d0e49f31cf
charles <tuanleducanh78202@gmail.com>
2026-03-20 00:54:20 +0700

    feat: add inline gitsign

--- lua/nvims/plugins/gitsigns.lua
+++ lua/nvims/plugins/gitsigns.lua
@@ -2,6 +2,13 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol",
+			delay = 500,
+			ignore_whitespace = false,
+		},
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
